6f9c722d7d11ca683e52013e062a628e8acb2df8
alexaldearroyo <alexaldearroyo@gmail.com>
2024-05-10 15:43:26 +0200

    Refactor config.ts

--- src/data/config.ts
+++ src/data/config.ts
@@ -1,12 +1,11 @@
 // src/data/config.ts
-const isProduction = process.env.NODE_ENV !== 'development';
-
 const config = {
   defaultTitle: 'Alex Arroyo',
-  url: isProduction
-    ? (process.env.NEXT_PUBLIC_PORTFOLIO_URL as string) ||
-      'https://default-url.com'
-    : 'http://alexarroyo-portfolio.netlify.app',
+  url:
+    process.env.NODE_ENV !== 'development'
+      ? (process.env.NEXT_PUBLIC_PORTFOLIO_URL as string) ||
+        'https://default-url.com'
+      : 'http://alexarroyo-portfolio.netlify.app',
   defaultDescription: 'I’m Alex Arroyo and I’m a Full-Stack Software Engineer!',
   googleAnalyticsID: 'G-PE8QW0ZGTZ',
   NEXT_PUBLIC_PORTFOLIO_URL: process.env.NEXT_PUBLIC_PORTFOLIO_URL as string,
